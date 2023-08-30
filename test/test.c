1af15f66ea2aa0d2dcc48038edf719929bd22f22
naegaos <johan.sebastian.hansen@gmail.com>
2023-08-30 08:29:35 +0200

    ree

--- test/test.c
+++ test/test.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
-main(){
-    printf("Hello World!\n");
+int main(){
+    int arr[10] = {0, 1, 2, 5};
+    for (int i = 1; i < sizeof(arr); i++){
+        printf("%d\n", *(arr + i - 1));
+    }
 }
